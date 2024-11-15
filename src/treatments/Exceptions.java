3bccf5f4daa573ec4c08e5623b6e0f699d7e3fe8
Maria Iasmin <miasmincsilva@gmail.com>
2024-11-15 19:42:36 -0300

    Add method to handle invalid value exceptions with user feedback

--- src/treatments/Exceptions.java
+++ src/treatments/Exceptions.java
@@ -1,4 +1,8 @@
 package treatments;
 
 public class Exceptions {
+  public static void valorInvalido() throws InterruptedException{
+    System.out.println("Insira um valor valido!");
+    Thread.sleep(2000);
+}
 }
